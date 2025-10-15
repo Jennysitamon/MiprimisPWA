e9fdb22971d0c73fbd82b6576bcde44e75b4ac47
Jenyfer Montiel <jenymontiel13@gmail.com>
2025-10-14 20:30:54 -0600

    agregaroffline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-//Estructura básica de un servicio worker
+//estructura basica delserviciworker
 
 //1. Nombre del caché y archivos a cachear
 const CACHE_NAME = "mi-cache-1";
@@ -12,24 +12,45 @@ const urlsToCache = [
   "icons/icon-512x512.png"
 ];
 
-//2.Install -> se ejecuta al instalat el SW
+//2. Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Archivos cacheaados correctamente");
+      console.log("Archivos cacheados correctamente");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-//3. activate -> se ejecuta al activarme (limpia caches viejas)
+//3. Activate
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys=>
-            Promise.all(
-                keys.filter(key=>key !== CACHE_NAME)
-                .map(key => caches.delete(key))
-            )
-        )
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+//4. FETCH -
+self.addEventListener("fetch", event => {
+
+  if (event.request.url.includes("login.html")) {
+    event.respondWith(
+      fetch(event.request)
+        .catch(() => caches.match("offline.html"))
     );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => caches.match("offline.html"));
+      })
+  );
 });
